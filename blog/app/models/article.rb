b646c7aac0324d006c57e32cbfa4ed7e615a92e7
krdprog <info@krdprog.ru>
2019-01-31 15:48:34 +0300

    Update article spec

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
+	validates :title, presence: true
+	validates :text, presence: true
 	has_many :comments
 end
--- blog/spec/models/article_spec.rb
+++ blog/spec/models/article_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 describe Article do
-	it { should have_many :comments }
+	describe "validations" do
+		it { should validate_presence_of :title }
+		it { should validate_presence_of :text }
+	end
+
+	describe "assotiations" do
+		it { should have_many :comments }
+	end
 end
