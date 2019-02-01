9be66039058d22e83ad43a568eecf74ca68a5d9d
krdprog <info@krdprog.ru>
2019-02-01 22:12:49 +0300

    Fix space in test

--- blog/spec/features/visitor_create_contact_spec.rb
+++ blog/spec/features/visitor_create_contact_spec.rb
@@ -1,11 +1,11 @@
 require "rails_helper"
 
 feature "Contact creation" do
-	scenario "allows acess to contacts page" do
-		visit new_contacts_path
+  scenario "allows acess to contacts page" do
+    visit new_contacts_path
 
-		expect(page).to have_content I18n.t('contacts.contact_us')
-	end
+    expect(page).to have_content I18n.t('contacts.contact_us')
+  end
 
   scenario "allows a guest to create contact" do
     visit new_contacts_path
