b75d8007f8d2955c7954a11e3a1ade1bef2dd35f
krdprog <info@krdprog.ru>
2019-02-01 18:52:16 +0300

    Add Contact test - send form

--- blog/spec/features/visitor_create_contact_spec.rb
+++ blog/spec/features/visitor_create_contact_spec.rb
@@ -6,4 +6,15 @@ feature "Contact creation" do
 
 		expect(page).to have_content I18n.t('contacts.contact_us')
 	end
+
+  scenario "allows a guest to create contact" do
+    visit '/contacts'
+    fill_in :contact_email, with: 'foo@bar.ru'
+    fill_in :contact_message, with: 'Foo Bar Baz'
+
+    click_button 'Send message'
+    expect(page).to have_content 'Thanks!'
+  end
+
+
 end
