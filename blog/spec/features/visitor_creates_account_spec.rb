5680c3e9881ee7a29cc83fd1aaa83bd10bf91c6d
krdprog <info@krdprog.ru>
2019-02-01 22:04:28 +0300

    Add full test for sign up form

--- blog/spec/features/visitor_creates_account_spec.rb
+++ blog/spec/features/visitor_creates_account_spec.rb
@@ -1,19 +1,15 @@
 require "rails_helper"
 
-feature "Sign Up creation" do
-  scenario "allows acess to sugn up page" do
+feature "Account Creation" do
+  scenario "allows guest to create account" do
     visit new_user_registration_path
 
-    expect(page).to have_content "Sign up"
+    fill_in :user_username, with: 'FooBar'
+    fill_in :user_email, with: 'foo@bar.com'
+    fill_in :user_password, with: '1234567'
+    fill_in :user_password_confirmation, with: '1234567'
+
+    click_button 'Sign up'
+    expect(page).to have_content 'Welcome!'
   end
-
-  # scenario "allows a guest to create contact" do
-  #   visit '/contacts'
-  #   fill_in :contact_email, with: 'foo@bar.ru'
-  #   fill_in :contact_message, with: 'Foo Bar Baz'
-
-  #   click_button 'Send message'
-  #   expect(page).to have_content 'Thanks!'
-  # end
-
 end
